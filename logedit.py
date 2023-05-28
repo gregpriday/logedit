8b7ae01cb0cf4672dbde23d1f68959a9b54379f6
Greg Priday <greg@siteorigin.com>
2023-05-28 19:10:47 +0200

    Tweaks and added GPT-4 option

--- logedit.py
+++ logedit.py
@@ -14,7 +14,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# define a function to send text to OpenAI API and get summary
+
 def summarize(text):
     system_message = open(os.path.join(script_dir, "./system/commit_summarizer.txt"), "r").read()
     completion = openai.ChatCompletion.create(
@@ -27,20 +27,24 @@ def summarize(text):
     return completion.choices[0].message['content']
 
 
-def main(current_version, changelog_file):
+def main(current_version, changelog_file, model="gpt-3.5-turbo"):
     # initialize git repo
     repo = Repo(os.getcwd())
 
-    # get all tags in repo
-    tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
+    if ':' in current_version:
+        previous_version, current_version = current_version.split(':')
+    else:
+        # get all tags in repo
+        tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
 
-    # find the most recent version
-    recent_version = tags[-1]
+        # find the most recent version
+        previous_version = tags[-1]
 
-    print(f"Most recent version is: {recent_version}")
+    print(f"Previous version is: {previous_version}")
+    print(f"Current version is: {current_version}")
 
     # get commits between recent version and current version
-    commits = list(repo.iter_commits(f'{recent_version}..HEAD'))
+    commits = list(repo.iter_commits(f'{previous_version}..{current_version}'))
 
     print(f"Total commits: {len(commits)}")
 
@@ -60,15 +64,15 @@ def main(current_version, changelog_file):
     system_message = open(os.path.join(script_dir, "./system/changelog_writer.txt"), "r").read()
     messages = [
         {"role": "system", "content": system_message},
-        {"role": "user", "content": "Here is the tail of my existing CHANGELOG.md. Please use this as a guide on format and style.\n\n===\n\n" + "".join(last_lines)},
+        {"role": "user", "content": "Here is the tail of the existing CHANGELOG.md. Please use this as a guide on format and style.\n\n===\n\n" + "".join(last_lines)},
         {"role": "user", "content": f"The new version is {current_version} and today's date is {datetime.now().date().isoformat()} (date format is ISO 8601 - YYYY-MM-DD)"},
-        {"role": "user", "content": f"I will now give you commit summaries for the commits between {recent_version} and {current_version} from oldest to newest:" + "\n\n---\n\n".join(summaries)},
-        {"role": "user", "content": "Please give me the new changelog entry for version the new version {current_version}, given these commit messages, following the format of my current CHANGELOG.md. Give only the new entry, nothing else. Please put the most significant changes first."}
+        {"role": "user", "content": f"I will now give you commit summaries for the commits between {previous_version} and {current_version} from oldest to newest:" + "\n\n---\n\n".join(summaries)},
+        {"role": "user", "content": f"Please give me the new changelog entry for version the new version {current_version}, given these commit messages, following the format of my current CHANGELOG.md. Give only the new entry, nothing else. Please put the most significant changes first."}
     ]
 
     print("Summarized commits, generating changelog entry...")
     completion = openai.ChatCompletion.create(
-        model="gpt-4",
+        model=model,
         messages=messages
     )
     new_changelog_entry = completion.choices[0].message['content']
@@ -80,6 +84,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Automatically generate a changelog entry from git commits.")
     parser.add_argument('current_version', type=str, default="HEAD", nargs="?", help="Current version of the software.")
     parser.add_argument('changelog_file', type=str, default="CHANGELOG.md", nargs="?", help="Path to the CHANGELOG.md file.")
+    parser.add_argument('--gpt4', action='store_true', help="Use GPT-4 model if specified, otherwise use GPT-3.5 Turbo.")
     args = parser.parse_args()
 
-    main(args.current_version, args.changelog_file)
+    model = "gpt-4" if args.gpt4 else "gpt-3.5-turbo"
+    main(args.current_version, args.changelog_file, model)
